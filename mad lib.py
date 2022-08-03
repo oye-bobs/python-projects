6bf0a40263fa2f31666a746890a437cd14542e53
oye-bobs <33554816+oye-bobs@users.noreply.github.com>
2022-08-03 17:37:20 +0100

    Create mad lib.py

---
+++ mad lib.py
@@ -0,0 +1,9 @@
+verb = input("verb: ")
+noun1 = input("noun: ")
+noun2 = input("noun: ")
+adj = input("Adjective: ")
+fav_celebrity = input("Favourite Celebrity: ")
+madlib = (f"to find yourself, you have to {verb}, this is because man's {noun1}, is to be the {adj}, and to achieve all his {noun2} \
+and become like {fav_celebrity}")
+
+print (madlib)
